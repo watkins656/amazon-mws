48fd65e8f5994635643231b5fc5ae2f68b21e267
Dustin Watkins <watkins656@gmail.com>
2018-11-19 05:22:26 -0600

    add jaws

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,13 @@
 let mySQLPassword = process.env.MYSQL_PASSWORD;
 // Set up MySQL connection.
 var mysql = require("mysql");
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-var connection = mysql.createConnection({
+
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -18,7 +23,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-console.log(mySQLPassword);
 
 // Export connection for our ORM to use.
 module.exports = connection;
