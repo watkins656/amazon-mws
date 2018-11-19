e275c2e77246b298b442919fbc200bbe0c9816f1
Dustin Watkins <watkins656@gmail.com>
2018-11-19 05:26:15 -0600

    fix

--- config/connection.js
+++ config/connection.js
@@ -3,26 +3,26 @@ let mySQLPassword = process.env.MYSQL_PASSWORD;
 var mysql = require("mysql");
 let connection;
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
 
- connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: mySQLPassword,
-  database: "amazon"
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: mySQLPassword,
+    database: "amazon"
+  });
+};
+  // Make connection.
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
+  // Export connection for our ORM to use.
+  module.exports = connection;
